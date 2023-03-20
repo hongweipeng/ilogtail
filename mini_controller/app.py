0f748aae45738625ee2a1c380a6da25f9a6f2ea8
洪伟鹏 <hongweipeng@bonree.com>
2023-03-20 20:07:27 +0800

    fix

--- mini_controller/app.py
+++ mini_controller/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import sls_logs_pb2
+
 app = Flask(__name__)
 
 
@@ -10,14 +10,6 @@ def before_request():
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-@app.route('/logstores/test_logstore/shards/lb', methods=['POST'])
-def test_logstore():
-    # body = request.data
-    # msg = sls_logs_pb2.LogGroup()
-    # msg.ParseFromString(body)
-    # print(str(msg))
-    return 'ok'
 
 
 if __name__ == '__main__':

7675cc2bd6b2be26aedf22ef1158792d8bd16a1f
洪伟鹏 <hongweipeng@bonree.com>
2023-03-20 19:56:42 +0800

    fix

--- plugins/all/all.go
+++ plugins/all/all.go
@@ -21,6 +21,7 @@ import (
 	_ "github.com/alibaba/ilogtail/plugins/aggregator/logstorerouter"
 	_ "github.com/alibaba/ilogtail/plugins/aggregator/shardhash"
 	_ "github.com/alibaba/ilogtail/plugins/aggregator/skywalking"
+	_ "github.com/alibaba/ilogtail/plugins/flusher/brout"
 	_ "github.com/alibaba/ilogtail/plugins/flusher/checker"
 	_ "github.com/alibaba/ilogtail/plugins/flusher/grpc"
 	_ "github.com/alibaba/ilogtail/plugins/flusher/kafka"
